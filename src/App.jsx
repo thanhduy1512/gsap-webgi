69e6119076f8d50c673f087288e09d5bddcff93c
Duong Thanh Duy <dexter@nexondv.com>
2023-06-19 15:31:04 +0700

    add iphone rotate animation and preview mode

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useRef } from 'react';
+import DisplaySection from './components/DisplaySection';
+import Jumbotron from './components/Jumbotron';
+import Nav from './components/Nav';
+import SoundSection from './components/SoundSection';
+import WebgiViewer from './components/WebgiViewer';
+
+function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  };
+
+  return (
+    <div className="App">
+      <div ref={contentRef} id="content">
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
+    </div>
+  );
+}
+
+export default App;
